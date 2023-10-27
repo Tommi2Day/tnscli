26773f7e2853cc8c24931861e8db874dc69f08b6
Tommi2Day <tommi2day@tommi2day.net>
2023-10-27 21:40:33 +0200

    refactor testfunc.go

--- test/testfunc.go
+++ test/testfunc.go
@@ -1,4 +1,4 @@
-// Package test defines path settings while testing
+// Package test init test directories
 package test
 
 // https://intellij-support.jetbrains.com/hc/en-us/community/posts/360009685279-Go-test-working-directory-keeps-changing-to-dir-of-the-test-file-instead-of-value-in-template
@@ -9,30 +9,13 @@ import (
 	"testing"
 )
 
-// TestDir working dir for tests
+// TestDir working dir for test
 var TestDir string
 
-// TestData directory for working files
+// TestData directory for working Attachments
 var TestData string
 
-func init() {
-	_, filename, _, _ := runtime.Caller(0)
-	dir := path.Dir(filename)
-	err := os.Chdir(dir)
-	if err != nil {
-		panic(err)
-	}
-	TestDir = dir
-	TestData = path.Join(TestDir, "testdata")
-	// create data directory and ignore errors
-	err = os.Mkdir(TestData, 0750)
-	if err != nil && !os.IsExist(err) {
-		panic(err)
-	}
-	println("Work in " + dir)
-}
-
-// Testinit alternative init Test directories
+// Testinit set test directory
 func Testinit(t *testing.T) {
 	_, filename, _, _ := runtime.Caller(0)
 	dir := path.Dir(filename)
